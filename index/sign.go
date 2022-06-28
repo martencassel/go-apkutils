001111e28fb8e14127b61c96bc7616e07e1128cc
martencassel <marten.cassel@gmail.com>
2022-06-28 18:55:20 +0200

    Sign APKINDEX wip

---
+++ index/sign.go
@@ -0,0 +1,61 @@
+package index
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+)
+
+// A Signer is can create signatures that verify against a public key.
+type Signer interface {
+	// Sign returns raw signature for the given data. This method
+	// will apply the hash specified for the keytype to the data.
+	Sign(data []byte) ([]byte, error)
+}
+
+type rsaPrivateKey struct {
+	*rsa.PrivateKey
+}
+
+func parsePrivateKey(pemBytes []byte) (Signer, error) {
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, errors.New("ssh: no key found")
+	}
+	var rawkey interface{}
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		rsa, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		rawkey = rsa
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+	}
+	return newSignerFromKey(rawkey)
+}
+
+func (r *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
+	h := sha1.New()
+	h.Write(data)
+	d := h.Sum(nil)
+	return rsa.SignPKCS1v15(rand.Reader, r.PrivateKey, crypto.SHA1, d)
+}
+
+// Sign a APKINDEX file.
+func newSignerFromKey(k interface{}) (Signer, error) {
+	var sshKey Signer
+	switch t := k.(type) {
+	case *rsa.PrivateKey:
+		sshKey = &rsaPrivateKey{t}
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
+	}
+	return sshKey, nil
+}
---
+++ index/sign_test.go
@@ -0,0 +1,131 @@
+package index
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+/*
+	openssl dgst -sha1 -sign ../testdata/my_key -out .SIGN.RSA.mykey ../testdata/APKINDEX.unsigned.tar.gz
+*/
+func TestSignApkIndex(t *testing.T) {
+	t.Run("Sign a APKINDEX file using private key and public key name", func(t *testing.T) {
+		unsignedIndex := "../testdata/APKINDEX.unsigned.tar.gz"
+		publicKeyName := "my_key"
+		signer, err := loadPrivateKey("../testdata/my_key")
+		if err != nil {
+			t.Fatal("Error loading private key:", err)
+		}
+		buf_unsignedApkIndexTarGz, err := ioutil.ReadFile(unsignedIndex)
+		if err != nil {
+			t.Fatal("Error reading unsigned index:", err)
+		}
+		signed, err := signer.Sign(buf_unsignedApkIndexTarGz)
+		if err != nil {
+			t.Fatal("Error signing:", err)
+		}
+		sig := base64.StdEncoding.EncodeToString(signed)
+		fmt.Printf("Encoded: %v\n", sig)
+		f, err := os.Create(fmt.Sprintf(".SIGN.RSA.%s", publicKeyName))
+		if err != nil {
+			t.Fatal("Error creating file:", err)
+		}
+		f.Write(signed)
+		f.Close()
+
+		// Put the signature in a tar file withouth the end-of-tar record at the end of the file.
+		sigTarFileWithouthEOF, err := os.Create("signature_w_eof.tar")
+		if err != nil {
+			t.Fatal("Error opening signature.tar.gz:", err)
+		}
+		fName := fmt.Sprintf(".SIGN.RSA.%s", publicKeyName)
+		log.Printf("Adding %v to signature.tar.gz", fName)
+		sigFile, err := os.Open(fName)
+		if err != nil {
+			t.Fatal("Error opening signature file:", err)
+		}
+		sigFileData, err := ioutil.ReadAll(sigFile)
+		log.Printf("Read %v bytes from signature file", len(sigFileData))
+		if err != nil {
+			t.Fatal("Error reading signature file:", err)
+		}
+		hdr := &tar.Header{
+			Name: fName,
+			Mode: 0644,
+			Size: int64(len(sigFileData)),
+		}
+		tarWriter := tar.NewWriter(sigTarFileWithouthEOF)
+		tarWriter.WriteHeader(hdr)
+		tarWriter.Write(sigFileData)
+		// Skip writing a EOF tar records
+		f, err = os.Open("signature_w_eof.tar")
+		if err != nil {
+			t.Fatal("Error opening signature_w_eof.tar:", err)
+		}
+		var buf_signatureTarGz bytes.Buffer
+		gzw := gzip.NewWriter(&buf_signatureTarGz)
+		b, err := ioutil.ReadAll(f)
+		if err != nil {
+			t.Fatal("Error reading signature_w_eof.tar:", err)
+		}
+		n, err := gzw.Write(b)
+		if err != nil {
+			t.Fatal("Error writing signature.tar.gz:", err)
+		}
+		if n != len(b) {
+			t.Fatal("Error writing signature.tar.gz:", err)
+		}
+		gzw.Flush()
+		gzw.Close()
+		out, err := os.Create("signature.tar.gz")
+		if err != nil {
+			t.Fatal("Error creating signature.tar.gz:", err)
+		}
+		out.Write(buf_signatureTarGz.Bytes())
+		out.Close()
+
+		outF, err := os.Create("APKINDEX.signed.tar.gz")
+		if err != nil {
+			t.Fatal("Error creating APKINDEX.signed.tar.gz:", err)
+		}
+		WriteApkIndex(outF, &buf_signatureTarGz, bytes.NewBuffer(buf_unsignedApkIndexTarGz))
+	})
+}
+
+func CreateApkIndexUnsigned(pkg ...io.Reader) (*bytes.Buffer, error) {
+	for _, apk := range pkg {
+		if apk == nil {
+			return nil, fmt.Errorf("apk is nil")
+		}
+	}
+}
+
+func WriteApkIndex(w io.Writer, signatureTarGz *bytes.Buffer, unsignedApkIndexTarGz *bytes.Buffer) {
+	w.Write(signatureTarGz.Bytes())
+	w.Write(unsignedApkIndexTarGz.Bytes())
+}
+
+func loadPrivateKey(path string) (Signer, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return parsePrivateKey(data)
+}
+
+// Create temp file
+func createTempFile(t *testing.T) (*os.File, string) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal("Error creating temp file:", err)
+	}
+	return f, f.Name()
+}
